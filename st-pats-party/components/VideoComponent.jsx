1bd11cd4633e165901afdd250d73d9d410bc4d5a
Mike Elsmore <mike@elsmore.me>
2021-02-26 17:02:49 +0000

    Allow the conductor to play at the correct point

--- st-pats-party/components/VideoComponent.jsx
+++ st-pats-party/components/VideoComponent.jsx
@@ -1,18 +1,29 @@
 
 import React, { useEffect, useState } from 'react';
 import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
-
+import Ably from "ably/promises";
 class VideoComponent extends React.Component {
     constructor(props) {
         super(props);
         this.vidRef = React.createRef();
+        this.channelName = "slainte";
+        this.ably = new Ably.Realtime.Promise({ authUrl: '/api/ablyTokenRequest' });
+        this.channel = this.ably.channels.get(this.channelName);
     }
 
     componentDidMount() {
         const video = this.vidRef.current; //get the video element
-        setInterval(() => {
-            // video.currentTime = 8
-        }, 4000)
+        // Individual events
+        this.channel.subscribe((message, err) => {
+            const { data } = message;
+            if (typeof data == 'string') {
+                const messageData = JSON.parse(data);
+                if (messageData.play) {
+                    video.currentTime = messageData.play;
+                    video.play();
+                }
+            }
+        });
     }
 
     render() {
@@ -20,7 +31,7 @@ class VideoComponent extends React.Component {
                 <Video
                     innerRef={this.vidRef}
                     publicId="st-pats-party/dog"
-                    controls="true"
+                    controls={true}
                 />
             </CloudinaryContext>);
     }
