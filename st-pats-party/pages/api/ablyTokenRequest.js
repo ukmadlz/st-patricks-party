cded6b49cf8321536e2f3f046ad5c4fc777c3c3e
Mike Elsmore <mike@elsmore.me>
2021-02-26 17:44:56 +0000

    Logging like...

--- st-pats-party/pages/api/ablyTokenRequest.js
+++ st-pats-party/pages/api/ablyTokenRequest.js
@@ -2,6 +2,7 @@ import Ably from 'ably/promises';
 
 export default async function handler(req, res) {
     if(!process.env.ABLY_KEY){
+        console.error('No config for Ably')
         res.status(500).json({
             message: 'No config for Ably'
         });
