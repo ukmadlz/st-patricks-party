993b8ab6f1def8f7a4bf389f698697aa3120ddcf
Mike Elsmore <mike@elsmore.me>
2021-02-26 17:33:33 +0000

    Make it more verbose for debugging

--- st-pats-party/pages/api/ablyTokenRequest.js
+++ st-pats-party/pages/api/ablyTokenRequest.js
@@ -1,6 +1,11 @@
 import Ably from 'ably/promises';
 
 export default async function handler(req, res) {
+    if(!process.env.ABLY_KEY){
+        res.status(500).json({
+            message: 'No config for Ably'
+        });
+    }
     const client = new Ably.Realtime(process.env.ABLY_KEY);
     const tokenRequestData = await client.auth.createTokenRequest({ clientId: 'st-pats-partay' });
     res.status(200).json(tokenRequestData);
