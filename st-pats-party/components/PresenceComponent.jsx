66721bacadcc80bf7496d37f6ba64993e0344af0
Mike Elsmore <mike@elsmore.me>
2021-03-17 17:33:50 +0000

    Improve the irish gbye logic so we know when to look for people

--- st-pats-party/components/PresenceComponent.jsx
+++ st-pats-party/components/PresenceComponent.jsx
@@ -6,6 +6,7 @@ import { faUser } from '@fortawesome/free-solid-svg-icons'
 class PresenceComponent extends React.Component {
     constructor(props) {
         super(props);
+        this.irishGoodbye = 300000;
         this.state = { members: [] };
         this.channelName = "slainte";
         this.ably = new Ably.Realtime.Promise({ authUrl: '/api/ablyTokenRequest' });
@@ -18,10 +19,16 @@ class PresenceComponent extends React.Component {
         // Individual events
         presence.subscribe((data, err) => {
             const { connectionId, action } = data;
-            const membersObject = { members: presence.members.map };
+            const membersObject = {
+                members: presence.members.map,
+                bounce: this.state.bounce || [],
+            };
             // Irish G'bye
             if(action == 'leave') {
-                membersObject.bounce = connectionId;
+                membersObject.bounce.push({
+                    connectionId,
+                    timestamp: (new Date()),
+                });
             }
             this.setState(membersObject);
         });
@@ -32,12 +39,15 @@ class PresenceComponent extends React.Component {
         // Use Ably's realtime presence detection
         const presence = this.channel.presence;
         // DAS BOOT FROM CHANNEL!
-        this.setState({ members: presence.members.map});
+        this.setState({
+            members: presence.members.map,
+            bounce: this.state.bounce,
+        });
         presence.unsubscribe();
     }
 
     render() {
-        const membersList = Object.values(this.state.members).map((member) => {
+        let membersList = Object.values(this.state.members).map((member) => {
             return <div
                 key={member.connectionId}
                 className="p-3 "
@@ -48,17 +58,25 @@ class PresenceComponent extends React.Component {
                 />
             </div>
         })
-        if (this.state.bounce) {
-            const bouncer = <div
-                key={this.state.bounce}
-                className="p-3 animate-spin"
-            >
-                <FontAwesomeIcon
-                    icon={faUser}
-                    size="2x"
-                />
-            </div>
-            membersList.push(bouncer);
+        if (this.state.bounce && this.state.bounce.length) {
+            const bouncer = this.state.bounce.filter((member) => {
+                const difference = (((new Date()).getTime() - member.timestamp.getTime()) < this.irishGoodbye)
+                console.log(difference);
+                return difference;
+            })
+            .map((member) => {
+                return <div
+                    key={member.connectionId}
+                    className="p-3 animate-spin"
+                >
+                    <FontAwesomeIcon
+                        icon={faUser}
+                        size="2x"
+                    />
+                </div>
+            });
+            console.log(membersList);
+            membersList = membersList.concat(bouncer);
         }
         return (
             <div className="flex justify-center pt-2">{membersList}</div>
